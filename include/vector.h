acf7e418bbacb55c47d8fbd254ae84a536ebffcd
Your Name <you@example.com>
2024-06-17 22:21:10 +0800

    some changes

--- include/vector.h
+++ include/vector.h
@@ -2,34 +2,35 @@
  * @file    : vector.h
  * @author  : theSparky Team
  * @version :
- * 
+ *
  * Functions for Vector operations.
  */
 
 #ifndef VECTOR_H
 #define VECTOR_H
 
-class Vector {
-	public:
-		int size;
-		double* values;
+class Vector
+{
+public:
+    int     size;
+    double* values;
 
-		Vector();
-		Vector(int n, double* values);
-		Vector(const Vector& x);
-		~Vector();
-		Vector& operator=(double a);
-		Vector& operator=(const Vector& x);
+    Vector();
+    Vector(int n, double* values);
+    Vector(const Vector& x);
+    ~Vector();
+    Vector& operator=(double a);
+    Vector& operator=(const Vector& x);
 
-		void Free();
-		void Resize(int n);
-		void Fill(double a) const;
-		void FillRandom() const;
-		void Copy(const Vector& x) const;
-		void Scale(double a) const;
-		void Shift(double a) const;
-		void AddScaled(double a, const Vector& x) const;
-		void Add2Scaled(double a, const Vector& x, double b, const Vector& y) const;	
+    void Free();
+    void Resize(int n);
+    void Fill(double a) const;
+    void FillRandom() const;
+    void Copy(const Vector& x) const;
+    void Scale(double a) const;
+    void Shift(double a) const;
+    void AddScaled(double a, const Vector& x) const;
+    void Add2Scaled(double a, const Vector& x, double b, const Vector& y) const;
 };
 
-#endif //VECTOR_H
+#endif  // VECTOR_H
